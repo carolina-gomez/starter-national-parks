8590c83b5a741390bc2c8a3c675541a291799727
Carolina Gomez <carolinagomezTX@gmail.com>
2021-10-13 12:39:35 -0500

    select elements exercise

---
+++ index.js
@@ -0,0 +1,16 @@
+// select elements on DOM
+const value = document.querySelector(".park-display");
+console.log(value)
+
+const button = document.querySelector("button")
+console.log(button)
+
+ const areaClass = document.querySelector(".area-display")
+ console.log(areaClass)
+
+ const classStat = document.querySelector(".stats")
+ const findDiv = classStat.querySelector("div")
+ console.log(findDiv)
+
+ const findHello = document.querySelector(".hello")
+ console.log(findHello)
