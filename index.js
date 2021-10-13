30cdf236d96e353d0e2435b1d02025607fa65d39
Carolina Gomez <carolinagomezTX@gmail.com>
2021-10-13 12:53:46 -0500

    querySelectorAll and iterate over NodeList

--- index.js
+++ index.js
@@ -19,3 +19,20 @@ console.log(button)
  const buttons = document.querySelectorAll("button")
  console.log(buttons)
 
+//you can iterate over the NodeList using the values() method
+
+//get a list of all <div> elements containing ratins on the page
+//log them using the values method
+const divsRatings = document.querySelectorAll(".rating-display");
+
+//iterate over the list and print each one
+for (let element of divsRatings.values()) {
+    console.log(element)
+}
+//get a list of all the div elements containing areas on the page
+//log them using the for loop
+const divsAreas = document.querySelectorAll(".area-display")
+for (let i = 0; i < divsAreas.length; i++) {
+    const element = divsAreas[i]
+    console.log(element)
+}
